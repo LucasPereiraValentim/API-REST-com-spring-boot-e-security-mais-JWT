71856bec73c3d56446dda41484f9becba541420a
Lucas Valentim <lucas.valentim.costa@gmail.com>
2021-08-06 16:57:44 -0300

    correcao de nome de classe e remocao da anotacao @EnableAutoConfiguration

--- src/main/java/br/com/projetospring/ProjetoSpringRestApiApplication.java
+++ src/main/java/br/com/projetospring/ProjetoSpringRestApiApplication.java
@@ -1,7 +1,6 @@
 package br.com.projetospring;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
@@ -18,7 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableTransactionManagement
 @RestController
 @EnableWebSecurity
-@EnableAutoConfiguration
 public class ProjetoSpringRestApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoSpringRestApiApplication.class, args);
